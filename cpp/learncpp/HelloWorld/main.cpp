df8934bc5adabe1862f83bba4a28ae9aab00bf6d
Tony Okeke <tonykabilanokeke@gmail.com>
2024-06-27 10:25:29 -0400

    clean dir

--- cpp/learncpp/HelloWorld/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream> // for std::cout
-
-int main()
-{
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
-}
--- cpp/learncpp/colortext.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    std::cout << "Colored text!";
-    return 0;
-}
